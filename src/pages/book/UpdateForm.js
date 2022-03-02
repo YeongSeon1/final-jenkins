f1f099618e429efa60872a98105fb8649acf8f98
YeongSeon1 <tjdudtjs1101@gmail.com>
2022-03-02 19:04:03 +0900

    a

--- src/pages/book/UpdateForm.js
+++ src/pages/book/UpdateForm.js
@@ -28,7 +28,7 @@ const UpdateForm = (props) => {
     }
   
     if(member.userEmail.length === 0){
-      alert("이메일을 입력해주세요.");
+      alert("이메일을 입력해주세요..");
       return navigate("/saveForm");
     }
   
